ca6de36520f72f96e95f5db6b668bb787c56fffb
FJ1227 <mukumo.dog@gmail.com>
2023-06-07 01:15:09 +0900

    DP L

--- practice/Educatinao_DP_Contest/L_Deque.go
+++ practice/Educatinao_DP_Contest/L_Deque.go
@@ -15,8 +15,43 @@ func main() {
 	fmt.Fscan(r, &n)
 
 	a := make([]int, n)
-	for i := 1; i < n+1; i++ {
+	for i := 0; i < n; i++ {
 		fmt.Fscan(r, &a[i])
 	}
 
+	dp := make([][]int, n+1)
+	for i := 0; i < n+1; i++ {
+		dp[i] = make([]int, n+1)
+	}
+	for i := 0; i < n+1; i++ {
+		dp[i][i] = 0
+	}
+
+	for width := 1; width <= n; width++ {
+		for l := 0; l+width <= n; l++ {
+			r := l + width
+			if width%2 == n%2 {
+				dp[l][r] = max(dp[l+1][r]+a[l], dp[l][r-1]+a[r-1])
+			} else {
+				dp[l][r] = min(dp[l+1][r]-a[l], dp[l][r-1]-a[r-1])
+			}
+		}
+	}
+
+	fmt.Fprintln(w, dp[0][n])
+
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
